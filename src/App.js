e49a6ec7dfba6ba66e2c964b22b1a23b73da534f
Shane Phillips <shane@loongphillips.com>
2022-05-04 18:54:47 -0400

    weather.main

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import "./App.css";
+import { useEffect, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const lat = 45.5031824;
+const lon = -73.5698065;
+const key = "e55d17952f7e09c6f4873cd19cec6be6";
+
+const App = () => {
+	const [weather, setWeather] = useState(null);
+	useEffect(() => {
+		fetch(
+			`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}`
+		)
+			.then((res) => res.json())
+			.then((data) => {
+				console.log(data);
+				console.log("WEATHER: ", data.weather[0]);
+				setWeather(data.weather[0]);
+			});
+	}, []);
+	return (
+		<>
+			<p>{weather && weather.main}</p>
+		</>
+	);
+};
 
 export default App;
